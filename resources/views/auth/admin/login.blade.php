ea6198306be77552487d8ef5a30b50140d1b93ea
Aman Sahu <amansahu2333924@gmail.com>
2023-08-09 12:13:28 +0530

    HR_0.35

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -40,6 +40,58 @@
     <!-- INTERNAL SWITCHER CSS -->
     <link href="{{ asset('assets/switcher/css/switcher.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/switcher/demo.css') }}" rel="stylesheet" />
+     <style>
+        // best seen at 1500px or less
+
+html, body { height: 100%; }
+body {
+  background:radial-gradient(ellipse at center, rgba(255,254,234,1) 0%, rgba(255,254,234,1) 35%, #B7E8EB 100%);
+  overflow: hidden;
+}
+
+.ocean {
+  height: 5%;
+  width:100%;
+  position:absolute;
+  bottom:0;
+  left:0;
+  background: #015871;
+}
+
+.wave {
+  background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x;
+  position: absolute;
+  top: -198px;
+  width: 6400px;
+  height: 198px;
+  animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
+  transform: translate3d(0, 0, 0);
+}
+
+.wave:nth-of-type(2) {
+  top: -175px;
+  animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
+  opacity: 1;
+}
+
+@keyframes wave {
+  0% {
+    margin-left: 0;
+  }
+  100% {
+    margin-left: -1600px;
+  }
+}
+
+@keyframes swell {
+  0%, 100% {
+    transform: translate3d(0,-25px,0);
+  }
+  50% {
+    transform: translate3d(0,5px,0);
+  }
+}
+     </style>
 
 </head>
 
@@ -48,25 +100,25 @@
     <div class="page responsive-log coming-bg">
         <div class="page-content p-7 m-0">
             <div class="container text-center">
-                <div class="display-2 mb-5 font-weight-semibold">Welcome to <span style="color: #0000ff"><b>FixHR</b></span>
+                <span class="avatar avatar-xxl bradius" style="background-image: url(assets/images/users/8.jpg)"></span>
+                <div class="display-4 mb-1 mt-3 font-weight-semibold">Welcome to <span style="color: #0000ff"><b><span style="color: #080808">Fix</span>HR</b></span>
                 </div>
-                <h1 class="h3  mb-3">Make Your Human Resource Online</h1>
-                <p class="h5 font-weight-normal mb-4 leading-normal">You may have mistyped the address or the page may have moved.</p>
+                <h1 class="h4">Make Your Human Resource Online</h1>
                 <div class="coming-footer">
                     <form class="card-body pt-1" id="login" name="login">
                         <div class="form-group">
                             <div class="input-group mb-4">
-                                <label for="login" class="fw-bold">Enter Phone Number</label>
+                                <label for="login" class="fw-bold">Enter Your Email</label>
                                 <div class="input-group">
                                     <a href="#" class="input-group-text">
-                                        <i class="fe fe-phone" aria-hidden="true"></i>
+                                        <i class="fe fe-mail" aria-hidden="true"></i>
                                     </a>
-                                    <input class="form-control" placeholder="eg. +91 xxxxx xxx58">
+                                    <input class="form-control" placeholder="eg. xyz@gmail.com">
                                 </div>
                             </div>
                         </div>
                         <div class="submit">
-                            <a class="btn btn-primary btn-block" href="#">Login</a>
+                            <a class="btn btn-primary btn-block btn-lg" href="#">Login</a>
                         </div>
                         <div class="text-center mt-3">
                             <p class="text-dark mb-0">Don't have account?<a class="text-primary ms-1"
@@ -86,6 +138,10 @@
                 </div>
             </div>
         </div>
+        <div class="ocean">
+            <div class="wave"></div>
+            <div class="wave"></div>
+          </div>
     </div>
 
 
