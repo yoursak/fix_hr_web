a532df46157d18c732f5ef115f6b62493c8b463a
Aman Sahu <amansahu2333924@gmail.com>
2023-08-11 15:18:54 +0530

    HR_0.42

--- resources/views/admin/attendance/emp_attendace.blade.php
+++ resources/views/admin/attendance/emp_attendace.blade.php
@@ -93,31 +93,50 @@
 
 @section('contents')
     <div class="row">
-        <div class="card">
+        <div class="col-xl-4">
             <div class="card">
-                <div class="row">
-                    <div class="col-xl-4">
-                        <div class="my-5">
-                            <h4 class="my-5">Timesheet<span class="fs-14 mx-3 text-muted">Tue, 08th Aug 2023</span></h4>
+                <div class="card-header border-bottom-0">
+                    <h4 class="my-5">Timesheet<span class="fs-14 mx-3 text-muted">Tue, 08th Aug 2023</span></h4>
+                    <div class="page-rightheader ms-auto d-md-flex">
+                        <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
+                            <a href="#" class="btn btn-primary btn-sm">Approve</a>
                         </div>
+                    </div>
+                </div>
+                <div class="card-body pb-5">
+                    <div class="col-sm-12 my-auto" style="height: 300px">
                         <div class="row mb-5 mt-5">
-                            <div class="col-md-4">
+                            <div class="col-4">
                                 <div class="pt-5 text-center">
                                     <h6 class="mb-1 fs-16 font-weight-semibold">09:30 AM</h6>
                                     <small class="text-muted fs-14">Punch In</small>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div class="chart-circle chart-circle-md" data-value="100" data-thickness="6"
+                            <div class="col-4">
+                                <div class="chart-circle chart-circle-md" data-value="100" data-thickness="8"
                                     data-color="#0dcd94">
                                     <div class="chart-circle-value text-muted">09:00 hrs</div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-4">
                                 <div class="pt-5 text-center">
                                     <h6 class="mb-1 fs-16 font-weight-semibold"> 06:30 PM</h6>
                                     <small class="text-muted fs-14">Punch Out</small>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <div class="col-6">
+                                <a href="#" data-bs-toggle="modal"
+                                data-bs-target="#PunchIn" class="my-auto">
+                                    <span class="avatar avatar-xxl" style="background-image: url(assets/images/users/7.jpg)"></span>
+                                </a>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" data-bs-toggle="modal"
+                                data-bs-target="#PunchOut" class="my-auto">
+                                    <span class="avatar avatar-xxl" style="background-image: url(assets/images/users/7.jpg)"></span>
+                                </a>
                             </div>
                         </div>
                         <div class="my-5">
@@ -133,64 +152,88 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4">
-                        <div class="">
-                            <h4 class="my-5">Statistics</h4>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4">
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="">
+                        <h4 class="my-5">Statistics</h4>
+                    </div>
+                    <div class="col-sm-12 mt-5" style="height: 300px">
+                        <div class="d-flex justify-content-between">
+                            <h6>Today</h6>
+                            <h6><b>2/8 hr</b></h6>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h6>Today</h6>
-                                <div class="progress progress-md mb-3">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-teal"
-                                        style="width: 15%">15%</div>
-                                </div>
-                                <h6>This Week</h6>
-                                <div class="progress progress-md mb-3">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-info"
-                                        style="width: 25%">25%</div>
-                                </div>
-                                <h6>This Month</h6>
-                                <div class="progress progress-md mb-3">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-yellow"
-                                        style="width: 50%">50%</div>
-                                </div>
-                                <h6>Remaining</h6>
-                                <div class="progress progress-md mb-3">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger"
-                                        style="width: 70%">40%</div>
-                                </div>
-                                <h6>Overtime</h6>
-                                <div class="progress progress-md">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-green"
-                                        style="width: 70%">40%</div>
-                                </div>
+                        <div class="progress progress-md mb-3">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-teal" style="width: 15%">
+                                15%
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <h6>This week</h6>
+                            <h6><b>2/48 hr</b></h6>
+                        </div>
+                        <div class="progress progress-md mb-3">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: 25%">
+                                25%
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <h6>This Month</h6>
+                            <h6><b>2/208 hr</b></h6>
+                        </div>
+                        <div class="progress progress-md mb-3">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-yellow"
+                                style="width: 50%">
+                                50%</div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <h6>Remaining</h6>
+                            <h6><b>204/208 hr</b></h6>
+                        </div>
+                        <div class="progress progress-md mb-3">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger"
+                                style="width: 70%">
+                                40%</div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <h6>Overtime</h6>
+                            <h6><b>2/208 hr</b></h6>
+                        </div>
+                        <div class="progress progress-md">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-green"
+                                style="width: 70%">40%
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4">
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="col-sm-12">
                         <div class="">
                             <h4 class="my-5">Timeline</h4>
                         </div>
-                        <div class="timeline">
+                        <div class="col-sm-12 mt-5" style="height: 300px; overflow:scroll">
                             <div class="tl-content tl-content-active">
                                 <div class="tl-header">
                                     <span class="tl-marker"></span>
-                                    <p class="tl-title">Punch In at <br><span class="text-muted fs-14 fw-light">09:59
+                                    <p class="tl-title">Punch In at10:00 AM | General Shift <br><span class="text-muted fs-14 fw-light"><i class="fa fa-map-marker mx-1"></i>Bhanpuri, Raipur(CG)- 492001
                                             AM</span>
                                     <p>
                                 </div>
                                 <div class="tl-header">
                                     <span class="tl-marker"></span>
-                                    <p class="tl-title">Punch In at <br><span class="text-muted fs-14 fw-light">09:59
+                                    <p class="tl-title">Punch Out at 07:00 AM | General Shift <br><span class="text-muted fs-14 fw-light"><i class="fa fa-map-marker mx-1"></i>Bhanpuri, Raipur(CG)- 492001
                                             AM</span>
                                     <p>
                                 </div>
-                                <div class="tl-header">
-                                    <span class="tl-marker"></span>
-                                    <p class="tl-title">Punch In at <br><span class="text-muted fs-14 fw-light">09:59
-                                            AM</span>
-                                    <p>
-                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -201,24 +244,20 @@
     <div class="row">
         <div class="col-xl-12 col-md-12 col-lg-12">
             <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-12 col-lg-5">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="form-label">Search by Date:</label>
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <div class="input-group-text">
-                                                    <i class="feather feather-calendar"></i>
-                                                </div>
-                                            </div><input class="form-control fc-datepicker" placeholder="DD-MM-YYYY"
-                                                type="text">
-                                        </div>
-                                    </div>
-                                </div>
+                <div class="card-header border-bottom-0">
+                    <h3 class="card-title">Statutory Expenses</h3>
+                    <div class="page-rightheader ms-auto d-md-flex">
+                        <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button type="button" class="btn btn-outline-primary btn-sm">Copy</button>
+                                <button type="button" class="btn btn-outline-primary btn-sm">CSV</button>
+                                <button type="button" class="btn btn-outline-primary btn-sm">Excel</button>
+                                <button type="button" class="btn btn-outline-primary btn-sm">PDF</button>
+                                <button type="button" class="btn btn-outline-primary btn-sm">Print</button>
                             </div>
+                        </div>
+                        <div class="input-group">
+                            <input class="form-control fc-datepicker mx-3" placeholder="DD-MM-YYYY" type="date">
                         </div>
                     </div>
                 </div>
@@ -259,4 +298,20 @@
             </div>
         </div>
     </div>
+
+        {{-- Punch Image --}}
+        <div class="modal fade" id="PunchIn">
+            <div class="modal-dialog modal-dialog-centered " role="document">
+                <div class="modal-content tx-size-sm">
+                    <img src="{{asset('imgs/selfie.jpg')}}" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="PunchOut">
+            <div class="modal-dialog modal-dialog-centered " role="document">
+                <div class="modal-content tx-size-sm">
+                    <img src="{{asset('imgs/selfie.jpg')}}" alt="">
+                </div>
+            </div>
+        </div>
 @endsection
